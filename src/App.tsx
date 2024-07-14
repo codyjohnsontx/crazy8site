7267623578cf51881796fdc041558534d4a9a1ed
Cody Johnson <codyjohnsontx@gmail.com>
2024-07-13 23:07:02 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Hello world</h1>
+      
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      
+      
+    </>
+  )
+}
+
+export default App

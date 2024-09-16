5a37ab2e7beecaea483461f093ac1f1c9101e049
Cody Johnson <codyjohnsontx@gmail.com>
2024-09-16 00:22:13 -0500

    add navbar, further tune router and navbar pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
 import '@fontsource/roboto';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Title from './components/Title';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import ImagePage from './components/ImagePage';
 import HomePage from './components/HomePage';
-
-
+import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ImagePage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<ImagePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
+
+// Layout component to handle conditional NavBar and Logo rendering
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+
+  return (
+    <div>
+      {/* Conditionally render NavBar */}
+      {location.pathname !== '/' && (
+        <>
+          <NavBar />
+          {/* Logo below the navbar */}
+          <div className="logo-container">
+            <img src="assets/crazy8logo.jpeg" alt="Logo" className="logo" />
+          </div>
+        </>
+      )}
+      <div className="content">{children}</div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+
+                <li>
+                    <Link to="/about" className="nav-link">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact" className="nav-link">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/schedule" className="nav-link">Schedule</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
